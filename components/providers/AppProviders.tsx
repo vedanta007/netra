96feb2570b4df0e9cd0e7aa69a0f6269e7634f1c
vedanta007 <vedantasingh007@gmail.com>
2024-12-22 10:44:27 +0530

    initialize Prisma setup with SQLite and create Workflow model

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "next-themes"
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
     return (
-        <ThemeProvider attribute='class' defaultTheme="system" enableSystem>{children}</ThemeProvider>
+        <ThemeProvider attribute='class' defaultTheme="system" enableSystem>
+            {children}
+        </ThemeProvider>
     )
 }
